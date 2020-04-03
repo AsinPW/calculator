b5d31480e0d92e62c985b3b192f16d1b43f831a6
AsinPW <asinn1923@gmail.com>
2020-04-03 23:13:33 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+##### TWO FUNCTION #####
+
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+
+result = int(number1) + int(number2) # +
+#result = int(number1) - int(number2) # -
+#result = int(number1) * int(number2) # *
+#result = int(number1) / int(number2) # /
+
+print("Result: " + str(result))
+
+##### FOUR FUNCTION #####
+
+numberOne = input("Enter the first number: ")
+numberTwo = input("Enter the second number: ")
+numberThree = input("Enter the third number: ")
+numberFour = input("Enter the fourth number: ")
+
+resultOne = int(numberOne) + int(numberTwo) + int(numberThree) + int(numberFour) # +
+#resultOne = int(numberOne) - int(numberTwo) - int(numberThree) - int(numberFour) # -
+#resultOne = int(numberOne) * int(numberTwo) * int(numberThree) * int(numberFour) # *
+#resultOne = int(numberOne) / int(numberTwo) / int(numberThree) / int(numberFour) # /
+
+print("Result: " + str(resultOne))
+print("github.com/AsinPW")
